c6d7464ce60a93624b5895b1ef8cf2f19b01e9cf
Mateusvspm <sousamateusv@gmail.com>
2024-09-17 10:23:22 -0300

    Fazendo...

--- ContaBancaria.test.ts
+++ ContaBancaria.test.ts
@@ -1,4 +1,30 @@
-// import { beforeEach, describe, expect, test } from "bun:test";
-// import ContaBancaria from "./ContaBancaria.ts";
+import { beforeEach, describe, expect, test } from "bun:test";
+import ContaBancaria from "./ContaBancaria.ts";
+
+describe ("Teste de classe ContaBancaria", () => {
+    let conta: ContaBancaria = new ContaBancaria
+    beforeEach(() => {
+        conta = new ContaBancaria()
+    })
+    
+    test("Método Depositar", () => {
+    
+     expect(conta.depositar(50)).toBe(50)
+    })
+
+    test("Método Depositar - deposito inválido", () => {
+     expect(() => conta.depositar(-10)).toThrow("Valor inválido")
+    })
+
+    test("Método Sacar", () => {
+        conta.depositar(50)
+        expect(conta.sacar(10)).toBe(40)
+    })
+
+    test("Método Sacar - saque inválido", () => {
+      conta.depositar(50)
+      expect(() => conta.sacar(60)).toThrow("Valor inválido")
+    })
+})
 
 
--- ContaBancaria.ts
+++ ContaBancaria.ts
@@ -1,1 +1,38 @@
-export default class ContaBancaria {}
+export default class ContaBancaria {
+    private numeroConta: number
+    private agencia: number
+    private saldo: number
+    private extrato: string[]
+
+
+    public constructor(){
+        this.numeroConta = 0
+        this.agencia = 0
+        this.saldo = 0
+        this.extrato = []
+    }
+
+
+public depositar(valor: number){
+    if (valor>0){
+        this.saldo += valor
+        this.extrato.push(`Depósito de R$ ${valor.toFixed(2)}`)
+        return this.saldo
+    }else{
+        throw new Error("Valor inválido")
+    }
+
+    }
+
+public sacar(valor: number){
+    if (valor>0 && valor<=this.saldo){
+        this.saldo -= valor
+        this.extrato.push(`Depósito de R$ ${valor.toFixed(2)}`)
+        return this.saldo
+    }else{
+        throw new Error("Valor inválido")
+    }
+
+}
+
+}
